1a2bf09cb711b6bc5501450e5b6c86de8a1799f1
belajarngaji <ffarid.5213@gmail.com>
2025-09-07 13:55:54 +0700

    Update account.js

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'URL_PROYEK_ANDA';
-const SUPABASE_ANON_KEY = 'KUNCI_ANON_ANDA';
+const SUPABASE_URL = 'https://jpxtbdawajjyrvqrgijd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI4OTgsImV4cCI6MjA3MTg4ODg5OH0.vEqCzHYBByFZEXeLIBqx6b40x6-tjSYa3Il_b2mI9NE';
 
 const { createClient } = supabase;
 const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
