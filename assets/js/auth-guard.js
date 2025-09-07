ef9c31856314e30dc2564724dfe1bf5987570db5
belajarngaji <ffarid.5213@gmail.com>
2025-09-07 13:56:34 +0700

    Update auth-guard.js

--- assets/js/auth-guard.js
+++ assets/js/auth-guard.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL_GUARD = 'URL_PROYEK_ANDA';
-const SUPABASE_ANON_KEY_GUARD = 'KUNCI_ANON_ANDA';
+const SUPABASE_URL_GUARD = 'https://jpxtbdawajjyrvqrgijd.supabase.co';
+const SUPABASE_ANON_KEY_GUARD = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI4OTgsImV4cCI6MjA3MTg4ODg5OH0.vEqCzHYBByFZEXeLIBqx6b40x6-tjSYa3Il_b2mI9NE';
 
 // --- TAMBAHAN BARU ---
 // Atur ke 'true' saat Anda sedang coding, 
